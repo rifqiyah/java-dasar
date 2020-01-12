4c2fd429982939038eac39310216427b3065f486
rifqiyah <56857001+rifqiyah@users.noreply.github.com>
2020-01-12 13:55:22 +0700

    Add files via upload

---
+++ Exceptions/src/TryAndCatch/MyClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TryAndCatch;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class MyClass {
+    public static void main (String [] args){
+    int[] myNumbers = {1, 2, 3};
+    System.out.println(myNumbers[1]); // error!
+  }
+}
